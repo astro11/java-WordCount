43e80ba79ade51a11f5627072664cea07f2e3089
astro11 <aquila.strother@asu.edu>
2019-02-05 17:04:41 -0500

    init commit

---
+++ src/wordCount/Main.java
@@ -0,0 +1,11 @@
+package wordCount;
+
+public class Main
+    {
+        public static void main(String[] args)
+        {
+            String myWord = "init commit";
+
+            System.out.println("\n***** Word Count ******");
+        }
+    }
